37f1373322baf03ffe03b37036a8f70da1f3e2c5
sumiso_c0db8c <220422087+sumisoc0db8c@users.noreply.github.com>
2025-07-12 09:11:04 +0900

    すみそー！bot作成

---
+++ index.ts
@@ -0,0 +1,44 @@
+import { GatewayIntentBits, Client, Message, Events } from "discord.js";
+
+const { token } = require("./config.json");
+
+// 権限を指定してクライアントを作成
+const client = new Client({
+    intents: [
+        // メッセージの読み取り
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+// 起動確認
+client.once(Events.ClientReady, () => {
+    console.log("Ready!");
+    if (client.user) {
+        console.log(client.user.tag);
+    }
+});
+
+// メッセージ応答
+client.on(Events.MessageCreate, async (message: Message) => {
+    // botのメッセージには反応しない
+    if (message.author.bot) return;
+
+    if (message.content === "すみそー！") {
+        message.reply("すみそー！");
+    }
+});
+
+// discord接続
+client.login(token);
+
+// koyeb用のWebサーバー
+const port = process.env.PORT || 3000
+console.log(`Launching Bun HTTP server on port: ${port}, url: http://0.0.0.0:${port} 🚀`)
+Bun.serve({
+  port: port,
+  fetch(_request) {
+    return new Response('Hello from Koyeb')
+  },
+})
